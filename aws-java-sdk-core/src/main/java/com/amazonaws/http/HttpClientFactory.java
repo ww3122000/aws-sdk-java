39fe75009a145a5c6c07a0e6ab563906bc554f99
wangwei14 <wangwei14@kingsoft.com>
2016-07-18 18:37:50 +0800

    修改底层 httpClient 获取连接的方式。

--- aws-java-sdk-core/src/main/java/com/amazonaws/http/HttpClientFactory.java
+++ aws-java-sdk-core/src/main/java/com/amazonaws/http/HttpClientFactory.java
@@ -28,51 +28,35 @@ import javax.net.ssl.SSLSocket;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
-import org.apache.http.Header;
-import org.apache.http.HeaderElement;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpException;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpResponseInterceptor;
 import org.apache.http.ProtocolException;
-import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.ChallengeState;
-import org.apache.http.auth.NTCredentials;
 import org.apache.http.client.AuthCache;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.RedirectStrategy;
-import org.apache.http.client.entity.GzipDecompressingEntity;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.config.RequestConfig.Builder;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.protocol.ClientContext;
 import org.apache.http.conn.ConnectTimeoutException;
-import org.apache.http.conn.params.ConnRoutePNames;
-import org.apache.http.conn.params.ConnRouteParams;
-import org.apache.http.conn.scheme.PlainSocketFactory;
-import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeLayeredSocketFactory;
-import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.scheme.SchemeSocketFactory;
 import org.apache.http.conn.ssl.SSLContexts;
 import org.apache.http.conn.ssl.SSLInitializationException;
-import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.BasicAuthCache;
-import org.apache.http.impl.conn.PoolingClientConnectionManager;
-import org.apache.http.params.BasicHttpParams;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.protocol.HttpContext;
 
 import com.amazonaws.ClientConfiguration;
-import com.amazonaws.SDKGlobalConfiguration;
 import com.amazonaws.annotation.SdkInternalApi;
-import com.amazonaws.http.conn.SdkConnectionKeepAliveStrategy;
-import com.amazonaws.http.conn.ssl.SdkTLSSocketFactory;
-import com.amazonaws.http.impl.client.HttpRequestNoRetryHandler;
-import com.amazonaws.http.impl.client.SdkHttpClient;
 
 /** Responsible for creating and configuring instances of Apache HttpClient4. */
 @SdkInternalApi
@@ -87,127 +71,24 @@ public class HttpClientFactory {
      * @return The new, configured HttpClient.
      */
     public HttpClient createHttpClient(ClientConfiguration config) {
-        /* Set HTTP client parameters */
-        HttpParams httpClientParams = new BasicHttpParams();
-        HttpConnectionParams.setConnectionTimeout(httpClientParams, config.getConnectionTimeout());
-        HttpConnectionParams.setSoTimeout(httpClientParams, config.getSocketTimeout());
-        HttpConnectionParams.setStaleCheckingEnabled(httpClientParams, true);
-        HttpConnectionParams.setTcpNoDelay(httpClientParams, true);
-        HttpConnectionParams.setSoKeepalive(httpClientParams, config.useTcpKeepAlive());
-
-        int socketSendBufferSizeHint = config.getSocketBufferSizeHints()[0];
-        int socketReceiveBufferSizeHint = config.getSocketBufferSizeHints()[1];
-        if (socketSendBufferSizeHint > 0 || socketReceiveBufferSizeHint > 0) {
-            HttpConnectionParams.setSocketBufferSize(httpClientParams,
-                    Math.max(socketSendBufferSizeHint, socketReceiveBufferSizeHint));
-        }
-        final SSLContext sslContext = createSSLContext(config);
-        SSLSocketFactory sslSocketFactory =
-                config.getApacheHttpClientConfig().getSslSocketFactory();
-        if (sslSocketFactory == null) {
-            sslSocketFactory = new SdkTLSSocketFactory(
-                    sslContext,
-                    SSLSocketFactory.STRICT_HOSTNAME_VERIFIER);
-        }
-
-        PoolingClientConnectionManager connectionManager = ConnectionManagerFactory
-            .createPoolingClientConnManager(config, httpClientParams, sslSocketFactory);
-
-        SdkHttpClient httpClient = new SdkHttpClient(connectionManager, httpClientParams);
-        httpClient.setHttpRequestRetryHandler(HttpRequestNoRetryHandler.Singleton);
-        httpClient.setRedirectStrategy(new NeverFollowRedirectStrategy());
-
-        if (config.getConnectionMaxIdleMillis() > 0) {
-            httpClient.setKeepAliveStrategy(new SdkConnectionKeepAliveStrategy(
-                    config.getConnectionMaxIdleMillis()));
-        }
-
-        if (config.getLocalAddress() != null) {
-            ConnRouteParams.setLocalAddress(httpClientParams, config.getLocalAddress());
-        }
-
-        Scheme http = new Scheme("http", 80, PlainSocketFactory.getSocketFactory());
-        Scheme https = new Scheme("https", 443, sslSocketFactory);
-        SchemeRegistry sr = connectionManager.getSchemeRegistry();
-        sr.register(http);
-        sr.register(https);
-
-        /*
-         * If SSL cert checking for endpoints has been explicitly disabled,
-         * register a new scheme for HTTPS that won't cause self-signed certs to
-         * error out.
-         */
-        if (SDKGlobalConfiguration.isCertCheckingDisabled()) {
-            Scheme sch = new Scheme("https", 443, new TrustingSocketFactory());
-            httpClient.getConnectionManager().getSchemeRegistry().register(sch);
-        }
-
-        /* Set proxy if configured */
+    	PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
+		
+		Builder requestConfigBuilder = RequestConfig.custom().setConnectTimeout(config.getConnectionTimeout())
+				.setConnectionRequestTimeout(config.getRequestTimeout())
+				.setSocketTimeout(config.getSocketTimeout());
+		
+		/* Set proxy if configured */
         String proxyHost = config.getProxyHost();
         int proxyPort = config.getProxyPort();
         if (proxyHost != null && proxyPort > 0) {
             AmazonHttpClient.log.info("Configuring Proxy. Proxy Host: " + proxyHost + " " + "Proxy Port: " + proxyPort);
             HttpHost proxyHttpHost = new HttpHost(proxyHost, proxyPort);
-            httpClient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxyHttpHost);
-
-            String proxyUsername    = config.getProxyUsername();
-            String proxyPassword    = config.getProxyPassword();
-            String proxyDomain      = config.getProxyDomain();
-            String proxyWorkstation = config.getProxyWorkstation();
-
-            if (proxyUsername != null && proxyPassword != null) {
-                httpClient.getCredentialsProvider().setCredentials(
-                        new AuthScope(proxyHost, proxyPort),
-                        new NTCredentials(proxyUsername, proxyPassword, proxyWorkstation, proxyDomain));
-            }
-
-            // Add a request interceptor that sets up proxy authentication pre-emptively if configured
-            if (config.isPreemptiveBasicProxyAuth()){
-                httpClient.addRequestInterceptor(new PreemptiveProxyAuth(proxyHttpHost), 0);
-            }
+            requestConfigBuilder.setProxy(proxyHttpHost);
         }
-
-        /* Accept Gzip response if configured */
-        if (config.useGzip()) {
-
-            httpClient.addRequestInterceptor(new HttpRequestInterceptor() {
-
-                @Override
-                public void process(final HttpRequest request,
-                        final HttpContext context) throws HttpException,
-                        IOException {
-                    if (!request.containsHeader("Accept-Encoding")) {
-                        request.addHeader("Accept-Encoding", "gzip");
-                    }
-                }
-
-            });
-
-            httpClient.addResponseInterceptor(new HttpResponseInterceptor() {
-
-                @Override
-                public void process(final HttpResponse response,
-                        final HttpContext context) throws HttpException,
-                        IOException {
-                    HttpEntity entity = response.getEntity();
-                    if (entity != null) {
-                        Header ceheader = entity.getContentEncoding();
-                        if (ceheader != null) {
-                            HeaderElement[] codecs = ceheader.getElements();
-                            for (int i = 0; i < codecs.length; i++) {
-                                if (codecs[i].getName()
-                                        .equalsIgnoreCase("gzip")) {
-                                    response.setEntity(new GzipDecompressingEntity(
-                                            response.getEntity()));
-                                    return;
-                                }
-                            }
-                        }
-                    }
-                }
-
-            });
-        }
+        
+		RequestConfig requestConfig = requestConfigBuilder.build();
+		CloseableHttpClient httpClient = HttpClients.custom().setConnectionManagerShared(true).setConnectionManager(cm)
+				.setDefaultRequestConfig(requestConfig).build();
 
         return httpClient;
     }
